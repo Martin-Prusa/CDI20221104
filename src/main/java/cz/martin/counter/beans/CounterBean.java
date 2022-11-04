fd8f81413841fe6cff628c466bd87dd6a826380c
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-11-04 11:14:12 +0100

    saving

--- src/main/java/cz/martin/counter/beans/CounterBean.java
+++ src/main/java/cz/martin/counter/beans/CounterBean.java
@@ -3,16 +3,28 @@ package cz.martin.counter.beans;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
 
+import java.io.*;
+
 @Named("counter")
 @ApplicationScoped
 public class CounterBean {
     private int count = 0;
 
-    public void increment() {
+    public void increment() throws IOException {
         count++;
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("data.txt"));
+        bw.write(String.valueOf(count));
+        bw.close();
     }
 
     public int getCount() {
-        return count;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            this.count = Integer.parseInt(br.readLine());
+            return count;
+        } catch (IOException e) {
+            return 0;
+        }
     }
 }
